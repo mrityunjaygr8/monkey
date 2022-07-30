a2e1e6e5eedf5e6fbf140ba913db92a5348efc8d
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-07-30 18:00:22 +0530

    extra packages added to env.nix

--- env.nix
+++ env.nix
@@ -1,7 +1,18 @@
 { pkgs ? import <nixpkgs> {}}:
  pkgs.mkShell {
     buildInputs = with pkgs; [
+      exa
+      fd
+      ripgrep
+      bat
       go
+      gopls
       lazygit
+      difftastic
     ];
+
+    shellHook = ''
+      export GIT_EXTERNAL_DIFF=difft
+      alias ll="exa -altaccessed"
+    '';
    }
